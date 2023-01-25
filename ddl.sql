41435b706f53b3cb319c2c4ab0009735db634337
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-01-25 22:02:47 +0100

    ddl.sql acabado

--- ddl.sql
+++ ddl.sql
@@ -1,10 +1,13 @@
+/*CREAMOS LA BASE DE DATOS*/
 create database Assi_04_06;
 
+/* USAMOS ESTE COMANDO PARA EMPEZAR A TRABAJAR CON LA BASE DE DATOS*/
 use Assi_04_06;
-/*CREAMOS LAS TABLAS CON LAS PRIMARY KEY*/
+
+/*CREAMOS LAS TABLAS CON LAS PRIMARYS KEYS*/
 create table Employee (
     employee_id int not null,
-    primary key (employee_id),
+    primary key (employee_id)
 );
 
 create table Orders (
@@ -14,36 +17,47 @@ create table Orders (
 
 create table Restaurant (
     restaurant_id int not null,
-    primary key (restaurant_id),
+    primary key (restaurant_id)
 );
 
-/*AÑADIMOS EL RESTO DE ATRIBUTOS A LAS TABLAS*/
+/*AÑADIMOS EL RESTO DE ATRIBUTOS A LAS TABLAS Y LAS FOREIGNS KEYS*/
 
 /*PARA EMPLOYEE*/
-alter table Employee add (
-    full_name vchar(35) not null,
-    salary int not null,
-    email vchar(100),
-    phone_num int not null,
-    restaurant_id int not null,
-    foreign key (restaurant_id) references Restaurant (restaurant_id)
-);
+alter table Employee add (full_name varchar(35) not null);
+alter table Employee add (salary int not null);
+alter table Employee add (email varchar(100));
+alter table Employee add (phone_num int not null);
+alter table Employee add (restaurant_id int not null);
+alter table Employee add foreign key (restaurant_id) references Restaurant(restaurant_id);
+
 
 /*PARA ORDERS*/
+alter table Orders add (employee_id int not null);
+alter table Orders add (order_date date not null);
+alter table Orders add (order_price numeric(5,2) not null);
+alter table Orders add foreign key (employee_id) references Employee (employee_id);
 
-alter table Orders add (
-    employee_id int not null,
-    order_date date not null,
-    order_price numeric(5,2) not null,
-    foreign key (employee_id) references Employee (employee_id)
-);
+
 
 /*PARA RESTAURANT*/
+alter table Restaurant add (order_id int not null);
+alter table Restaurant add (direction varchar(20) not null);
+alter table Restaurant add (postal_code int not null);
+alter table Restaurant add (total_employees int not null);
+alter table Restaurant add foreign key (order_id) references Orders (order_id);
 
-alter table Restaurant add (
-    order_id int not null,
-    direction vchar(20) not null,
-    postal_code int not null,
-    total_employees int not null,
-    foreign key (order_id) references Orders (order_id)
-)
+
+/*PARA COMPROBAR QUE TODO SE HA CREADO CORRECTAMENTE, EJECUTAREMOS 2 COMANDOS EN TOTAL:*/
+
+/*COMANDO 1. PARA COMPROBAR LA CORRECTA CREACION DE LAS TABLAS*/
+show tables;
+
+/*COMANDO 2. PARA COMPROBAR LOS ATRIBUTOS DE LAS TABLAS Y LAS FOREIGN KEYS*/
+describe Employee;
+
+describe Orders;
+
+describe Restaurant;
+
+
+
